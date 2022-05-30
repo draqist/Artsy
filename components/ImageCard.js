cf8d38740f4c6be64e01f407c9b099ebf87f50b7
Draqode <Boomjay008@gmail.com>
2022-05-30 23:13:35 +0100

    feat: developed the image card component and also the projectcard component, adjusted the layout a little bit and wrote my bio

--- components/ImageCard.js
+++ components/ImageCard.js
@@ -4,8 +4,8 @@ import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 const ImageCard = () => {
   return (
-    <Box w='calc(100%)' border='2px solid ' p='6px' bgColor='linear-gradient(`180deg,rgba(35,37,49,.5),rgba(15,15,24,.5))'  backdropFilter='blur(104px)' borderRadius='8px' borderRadius='10px'>
-      <Flex justifyContent='center' w='100%'>
+    <Box w='calc(100%)' border='2px solid ' p='6px' bgColor='linear-gradient(`180deg,rgba(35,37,49,.5),rgba(15,15,24,.5))'  backdropFilter='blur(104px)'  borderRadius='8px'>
+      <Flex justifyContent='center' borderRadius='8px' w='100%'>
         <Image src='/profile3.jpeg'   borderRadius='8px' alt='profile-image'/>
       </Flex>
     </Box>
